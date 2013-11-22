23b37dc4178ab063ab9250f1fbc9ba3d742c9cc0
Scott Cagno <scottiecagno@gmail.com>
2013-11-21 22:24:54 -0500

    rc1

--- main/bench.go
+++ main/bench.go
@@ -5,10 +5,49 @@
 
 package main
 
-import "db"
-import "fmt"
+import (
+	"db"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
 
-// main benchmarker implementation
+const (
+	REQUESTS = 100000
+	WORKERS  = 10
+)
+
+func client() {
+	conn, err := net.Dial("tcp", "localhost:9000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	enc, dec := json.NewEncoder(conn), json.NewDecoder(conn)
+	for i := 0; i < REQUESTS; i++ {
+		enc.Encode(db.Msg{Cmd: "set", Key: fmt.Sprintf("%d", i), Val: true})
+		var m db.Msg
+		dec.Decode(&m)
+		if m.Val == false {
+			log.Fatal("woops")
+		}
+	}
+	conn.Close()
+}
+
 func main() {
-	db.InitBench(fmt.Sprintf("localhost:%d", *db.LISTEN)).Run(10)
+	var wg sync.WaitGroup
+	wg.Add(WORKERS)
+	t := time.Now().Unix()
+	for i := 0; i < WORKERS; i++ {
+		go func() {
+			client()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	t = time.Now().Unix() - t
+	fmt.Printf("%v rps\n", float64((WORKERS*REQUESTS)/t))
 }
